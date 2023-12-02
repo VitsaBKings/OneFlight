eb27999ebfeeba06fd34f8fffa32ce676a0ef122
DESKTOP-Q4KP768\FruitSalad <julesthaddeus.cruz@uap.asia>
2023-12-02 13:11:24 +0800

    FlightBooking model

---
+++ oneflight/src/main/java/com/uap/oneflight/controller/SignupController.java
@@ -0,0 +1,10 @@
+package com.uap.oneflight.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SignupController {
+
+}
+
---
+++ oneflight/src/main/java/com/uap/oneflight/model/FlightBooking.java
@@ -0,0 +1,14 @@
+package com.uap.oneflight.model;
+
+public class FlightBooking {
+    int queueing_office_id = 0;
+    LocalDate creation_date = null;
+    String origin_location_code = "";
+	String destination_location = "";
+	String code = "";
+	String round_trip_tag = "";
+	int number_of_passenger = 0;
+	String reference_number = "";
+	int flight_offer_id = 0;				
+	float total_charged_amount = 0;
+}
